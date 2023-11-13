d4118ea6488f90ebaa0a1ab1c716bcc8865f7486
GiuliaCarangelo <tnhkagura@gmail.com>
2023-11-13 09:51:31 +0100

    fix price rounding

--- js/script.js
+++ js/script.js
@@ -11,26 +11,24 @@
 
 
 // L'utente inserisce i due valori richiesti
-const userLength = Number(parseFloat(prompt('Inserisci il numero di chilometri (km) che dovrai percorrere')).toFixed(2));
+const userLength = Number(parseFloat(prompt('Inserisci il numero di chilometri (km) che dovrai percorrere')));
 const userAge = parseInt(prompt('Inserisci l\'età del passeggero'));
 console.log(userLength);
 console.log(userAge);
 
-let ticketPrice = Number(parseFloat(userLength * 0.21).toFixed(2));
+let ticketPrice = Number(parseFloat(userLength * 0.21));
 
 if (userAge < 18) {
     let discount = (ticketPrice * 20) / 100;
     ticketPrice = ticketPrice - discount;
     
     console.log('Hai ottenuto lo sconto del 20%! -' + discount + '€.\n Ora il ticket costa: ' + ticketPrice + '€');
-}
-
-else if (userAge >= 65) {
+}else if (userAge >= 65) {
     let discount = (ticketPrice * 40) / 100;
     ticketPrice = ticketPrice - discount;
 
     console.log('Hai ottenuto lo sconto del 40%! -' + discount + '€.\n Ora il ticket costa: ' + ticketPrice + '€');
 }
 
-document.getElementById('finalprice').innerHTML = ticketPrice + " €";
+document.getElementById('finalprice').innerHTML = ticketPrice.toFixed(2) + " €";
 
